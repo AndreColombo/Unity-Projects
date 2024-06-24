4a71d4d852a130da40e6c291193b5f7274582523
AndreColombo <rm15019@fiap.com.br>
2024-06-24 14:57:42 -0300

    Le Canard

---
+++ Le Canard/Assets/Scripts/Missil.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missil : MonoBehaviour
+{
+    [SerializeField]
+    //cria uma vari�vel para a velocidade
+    private float velocidade = 1;
+
+    [SerializeField]
+    //cria uma vari�vel para guardar a posi��o y do meu objeto
+    private float variacaoDaPosicaoY = 1.75f;
+
+    private void Awake()
+    {
+        //quando meu objeto for criado, ele mudar� a posi��o baseado em um valor
+        //aleat�rio entre os intervalos do valor de y
+        this.transform.Translate(Vector3.up * Random.Range(variacaoDaPosicaoY, -variacaoDaPosicaoY));
+    }
+
+    private void Update()
+    {
+        //adiciona movimento para a esquerda dos obst�culos
+        this.transform.Translate(Vector3.left * velocidade * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D objetoQueEuColidi)
+    {
+        //Chamando o m�todo Destruir cada vez que bater com a barreira
+        this.Destruir();
+    }
+
+    public void Destruir()
+    {
+        Destroy(this.gameObject);
+    }
+}
